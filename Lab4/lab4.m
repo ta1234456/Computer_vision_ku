18cb369f57a70f88a8a814c83bf7acfaa13b7cd2
ta1234456 <nattawrj@ais.co.th>
2025-01-09 22:45:14 +0700

    AA

---
+++ Lab4/lab4.m
@@ -0,0 +1,18 @@
+folderPath = 'F:\GitSourceTree\Computer_vision_ku\Lab4\lab4-images'; 
+fileType = '*.jpg'; % ตัวอย่างสำหรับไฟล์ภาพ .jpg
+
+% ดึงรายชื่อไฟล์ที่ตรงกับนามสกุลที่ระบุ
+fileList = dir(fullfile(folderPath, fileType));
+fileCount = numel(fileList);
+for i = 1:fileCount 
+    tempim = imread(fileList(i).folder+"\"+fileList(i).name);
+    im_hsv = rgb2hsv(tempim);
+    bananaPixel = find(im_hsv(:,:,2) > 0.3 | im_hsv(:,:,2) <= 0.3);
+    R = tempim(:,:,1); G = tempim(:,:,2); B = tempim(:,:,3);
+    feature_vector(i,1) = mean(R(bananaPixel));
+    feature_vector(i,2) = mean(G(bananaPixel));
+    feature_vector(i,3) = mean(B(bananaPixel));
+end
+Z = linkage(feature_vector, 'complete', 'euclidean'); 
+c = cluster(Z, 'maxclust', 4); 
+scatter3(feature_vector(:,1), feature_vector(:,2), feature_vector(:,3),240,c,'fill');
