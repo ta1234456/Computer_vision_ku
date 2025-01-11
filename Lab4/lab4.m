e2090a5dee4df96121e34a8b755e67ef1dd8275a
nattawrj <nattawrj@ais.co.th>
2025-01-11 08:55:11 +0700

    update path mac

--- Lab4/lab4.m
+++ Lab4/lab4.m
@@ -1,11 +1,11 @@
-folderPath = 'F:\GitSourceTree\Computer_vision_ku\Lab4\lab4-images'; 
+folderPath = '/Users/mimoais/Downloads/KU /Computer Vision/Lab4/lab4-images'; 
 fileType = '*.jpg'; % ตัวอย่างสำหรับไฟล์ภาพ .jpg
 
 % ดึงรายชื่อไฟล์ที่ตรงกับนามสกุลที่ระบุ
 fileList = dir(fullfile(folderPath, fileType));
 fileCount = numel(fileList);
 for i = 1:fileCount 
-    tempim = imread(fileList(i).folder+"\"+fileList(i).name);
+    tempim = imread(fileList(i).folder+"/"+fileList(i).name);
     im_hsv = rgb2hsv(tempim);
     bananaPixel = find(im_hsv(:,:,2) > 0.3 | im_hsv(:,:,2) <= 0.3);
     R = tempim(:,:,1); G = tempim(:,:,2); B = tempim(:,:,3);
