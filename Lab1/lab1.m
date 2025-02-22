246b88bbd92b9fa5dc3acf2fdf84396ca0617e84
nattawrj <nattawrj@ais.co.th>
2025-02-22 12:12:44 +0700

    update code

--- Lab1/lab1.m
+++ Lab1/lab1.m
@@ -1,19 +1,11 @@
-load("scramble_code.mat");
-image = imread("scrambled_image.tif");
-imshow(image);
-imageGrayScale = rgb2gray(image);
-imshow(imageGrayScale);
-newimage =zeros(909,1170,"uint8");
-newimage2 = zeros(909,1170,"uint8");
+load('scramble_code.mat');
+scrambledImage = imread('scrambled_image.tif');
 
-j=1;
-while j <= 909
-    newimage(r(1,j),:)=imageGrayScale(j,:);
-    j = 1+j;
+[rows, cols, channels] = size(scrambledImage);
+decodedImage = zeros(rows, cols, channels, 'uint8');
+
+for channel = 1:channels
+    decodedImage(r, c, channel) = scrambledImage(:, :, channel);
 end
-i=1;
-while i <= 1170
-     newimage2(:,c(1,i))=newimage(:,i);
-     i = 1+i;
-end
-imshow(newimage2);
+
+imshow(decodedImage);
--- Lab7/lab7.m
+++ Lab7/lab7.m
@@ -8,13 +8,13 @@ points3 = detectMSERFeatures(grayI);
 
 inputImage = imread('starbucks35_dataset/21.jpg');
 grayInput = rgb2gray(inputImage); 
-points4 = detectSURFFeatures(grayInput);
-points5 = detectHarrisFeatures(grayInput);
+points4 = detectHarrisFeatures(grayInput);
+points5 = detectSURFFeatures(grayInput);
 points6 = detectMSERFeatures(grayInput);
 
 %gray ref
 [refFeatures,refValidPoints]= extractFeatures(grayI,points2);
-[inputFeatures, inputValidPoints] = extractFeatures(grayInput, points4);
+[inputFeatures, inputValidPoints] = extractFeatures(grayInput, points5);
 
 indexPairs = matchFeatures(refFeatures, inputFeatures);
 
@@ -23,21 +23,19 @@ matchedInputPoints = inputValidPoints(indexPairs(:, 2));
 
 
 figure;
-subplot(1, 4, 1); imshow(I);hold on; plot(points.selectStrongest(50));title('HarrisFeatures');
-subplot(1, 4, 2); imshow(I);hold on; plot(points2.selectStrongest(50));title('SURFFeatures');
-subplot(1, 4, 3); imshow(I);hold on; plot(points3, 'showEllipses', true);title('MSERFeatures');
-subplot(1, 4, 4); imshow(inputImage);hold on; plot(points4.selectStrongest(50));title('SURFFeatures');
+subplot(4, 1, 1); imshow(I);hold on; plot(points.selectStrongest(50));title('HarrisFeatures');
+subplot(4, 1, 2); imshow(I);hold on; plot(points2.selectStrongest(50));title('SURFFeatures');
+subplot(4, 1, 3); imshow(I);hold on; plot(points3, 'showEllipses', true);title('MSERFeatures');
+subplot(4, 1, 4); imshow(inputImage);hold on; plot(points5.selectStrongest(50));title('SURFFeatures');
 figure;
 subplot(1, 1, 1); showMatchedFeatures(I, inputImage, matchedRefPoints, matchedInputPoints, 'montage');title('Matched SURF Features');
 
 
 [refFeaturesHarris,refValidPointsHarris]= extractFeatures(grayI,points);
-[inputFeaturesHarris, inputValidPointsHarris] = extractFeatures(grayInput, points5);
-
-indexPairsHarris = matchFeatures(refFeaturesHarris, inputFeaturesHarris, 'MatchThreshold', 5);
-
-matchedRefPointsHarris = refValidPointsHarris(indexPairsHarris(:, 1));
-matchedInputPointsHarris = inputValidPointsHarris(indexPairsHarris(:, 2));
+[inputFeaturesHarris, inputValidPointsHarris] = extractFeatures(grayInput, points4);
+indexPairsHarris = matchFeatures(refFeaturesHarris, inputFeaturesHarris, 'Method','Threshold','MatchThreshold', 20);
+matchedRefPointsHarris = refValidPointsHarris(indexPairsHarris(:, 1),:);
+matchedInputPointsHarris = inputValidPointsHarris(indexPairsHarris(:, 2),:);
 
 figure;
 subplot(1, 1, 1); showMatchedFeatures(I, inputImage, matchedRefPointsHarris, matchedInputPointsHarris, 'montage');title('Matched Harris Features');
@@ -47,7 +45,7 @@ subplot(1, 1, 1); showMatchedFeatures(I, inputImage, matchedRefPointsHarris, mat
 [refFeaturesMSER,refValidPointsMSER]= extractFeatures(grayI,points3);
 [inputFeaturesMSER, inputValidPointsMSER] = extractFeatures(grayInput, points6);
 
-indexPairsMSER = matchFeatures(refFeaturesMSER, inputFeaturesMSER, 'MaxRatio', 0.5);
+indexPairsMSER = matchFeatures(refFeaturesMSER, inputFeaturesMSER, 'MaxRatio', 0.1,'MatchThreshold', 20);
 
 matchedRefPointsMSER= refValidPointsMSER(indexPairsMSER(:, 1));
 matchedInputPointsMSER = inputValidPointsMSER(indexPairsMSER(:, 2));
